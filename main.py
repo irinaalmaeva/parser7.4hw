7560f9af2db7bbbbc173d1dc22d54ba07ae20557
irinaalmaeva <almaeva.irina@gmail.com>
2024-07-15 22:05:24 +0300

    add browser

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 browser = webdriver.Chrome()
-browser.get('https://ru.wikipedia.org/w/index.php?fulltext=%D0%9D%D0%B0%D0%B9%D1%82%D0%B8&search=%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0&title=%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F%3A%D0%9F%D0%BE%D0%B8%D1%81%D0%BA&ns0=1')
-time.sleep(3)
+browser.get('https://ru.wikipedia.org/w/index.php?search=&title=%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%9F%D0%BE%D0%B8%D1%81%D0%BA&go=%D0%9F%D0%B5%D1%80%D0%B5%D0%B9%D1%82%D0%B8')
 
 search = browser.find_element(By.ID, "searchInput")
 # Спрашиваем у пользователя запрос
